842ba54fc8cd49dc546a62c24ac6bd61d848ff76
Gabriel Díaz <gda.m1712@gmail.com>
2024-06-01 21:39:04 -0400

    Added tables migrations

---
+++ database/migrations/2024_06_02_013736_add_last_movement_id_to_product_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('product', function (Blueprint $table) {
+            //
+            $table->foreignId('last_movement_id')->constrained('movement')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('product', function (Blueprint $table) {
+            //
+            $table->dropForeign(['last_movement_id']);
+        });
+    }
+};
